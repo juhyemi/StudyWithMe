96ca223a951458e175deca1caef8aa22cd8b3525
KIMJUHYE <114149911+juhyemi@users.noreply.github.com>
2023-09-02 01:55:56 +0900

    [Solved_주혜] k번째 수

---
+++ 20230830/q11004.java
@@ -0,0 +1,18 @@
+package StudyWithMe;
+import java.util.*;
+import java.io.*;
+public class q11004 {
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken())-1;
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		System.out.println(arr[k]);
+	}
+}
